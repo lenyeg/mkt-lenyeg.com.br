7ac702eefb45584c5de4330a4f9592ac3fb7c1d0
Lucas <lucas.ribeiro711@gmail.com>
2022-10-18 14:47:06 -0300

    chore: add vue eslint parser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,astro}"],
+  content: ["./src/**/*.{html,js,astro,vue}"],
   theme: {
     extend: {},
   },
