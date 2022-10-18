5f17e86deaacf1f19002ce0142971d444ff8f298
Lucas <lucas.ribeiro711@gmail.com>
2022-10-18 13:21:07 -0300

    chore: add vite and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
