c154b0e848bd619b2cb204b66ffe6bfb0ca124ce
Lucas <lucas.ribeiro711@gmail.com>
2022-10-20 13:27:58 -0300

    fix: header ctas position

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -9,7 +9,7 @@
             <div class="flex flex-1 items-center md:absolute md:inset-y-0 md:left-0">
               <div class="flex w-full items-center justify-between md:w-auto">
                 <a href="#">
-                  <span class="sr-only">Your Company</span>
+                  <span class="sr-only">Lenyeg</span>
                   <img draggable="false" class="h-8 w-auto sm:h-10" src="/images/logo-white.svg" alt="" />
                 </a>
                 <div class="-mr-2 flex items-center md:hidden">
@@ -22,11 +22,6 @@
             </div>
             <div class="hidden md:flex md:space-x-10">
               <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-medium text-gray-500 hover:text-gray-900">{{ item.name }}</a>
-            </div>
-            <div class="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
-              <span class="inline-flex rounded-md shadow">
-                <a href="#" class="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-600 hover:bg-gray-50">Log in</a>
-              </span>
             </div>
           </nav>
         </div>
@@ -48,7 +43,6 @@
               <div class="px-2 pt-2 pb-3">
                 <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">{{ item.name }}</a>
               </div>
-              <a href="#" class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100">Log in</a>
             </div>
           </PopoverPanel>
         </transition>
@@ -67,12 +61,12 @@
               <div class="block md:hidden">
                 <img draggable="false" src="/images/hero.svg" alt="">
               </div>
-              <div class="mt-5 max-w-md sm:flex justify-center md:justify-start md:mt-8">
-                <div class="rounded-md shadow">
-                  <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
+              <div class="mt-5 flex flex-col justify-center items-center gap-3 md:flex-row md:justify-start md:mt-8">
+                <div class="rounded-md w-fit shadow">
+                  <a href="#" class="flex  items-center justify-center rounded-full border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-700 md:py-4 md:px-10 md:text-lg">Comece agora</a>
                 </div>
-                <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                  <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">Live demo</a>
+                <div class="rounded-md w-fit shadow">
+                  <a href="#" class="flex  items-center justify-center rounded-full border border-solid border-white text-white px-8 py-3 text-base font-medium md:py-4 md:px-10 md:text-lg">Saiba mais</a>
                 </div>
               </div>
             </div>
