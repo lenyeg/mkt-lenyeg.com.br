a822327dfb67996ece32c2d927d4a79513fe122b
Lucas <lucas.ribeiro711@gmail.com>
2022-10-24 16:16:42 -0300

    fix: TheSelector features

--- src/components/TheSelector.vue
+++ src/components/TheSelector.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="hidden md:flex flex-col gap-4 flex-1 p-2">
-    <img draggable="false" :src="heroImg" width="620" class="rounded" />
+  <section class="hidden md:flex flex-col gap-4 flex-1 p-2 mx-auto">
+    <img draggable="false" :src="heroImg.src" width="620" class="rounded" />
     <div class="flex justify-around items-center gap-2">
       <img
         v-for="image of images"
@@ -8,7 +8,7 @@
         draggable="false"
         :src="image.src"
         :alt="image.alt"
-        class="rounded flex-1 w-36"
+        class="rounded flex-1 w-36 cursor-pointer"
         @click="changeImage(image)"
       />
     </div>
@@ -16,11 +16,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+import { defineComponent, ref, reactive } from "vue";
 
 export default defineComponent({
   setup() {
-    let heroImg = computed((): string => "/images/pvai-app-portfolio.png");
+    let heroImg = reactive({ src: "/images/pvai-app-portfolio.png" });
     const images = ref({
       imgOne: { src: "/images/pvai-app-portfolio.png", alt: "Paranavai.app" },
       imgTwo: { src: "/images/osmarjr-portfolio.png", alt: "Osmar Junior LP" },
@@ -28,9 +28,7 @@ export default defineComponent({
     });
 
     const changeImage = (image: any) => {
-      console.log("cliquei", image.src);
-      heroImg = image.src;
-      console.log("agora sou", heroImg);
+      return (heroImg.src = image.src);
     };
 
     return { heroImg, images, changeImage };
