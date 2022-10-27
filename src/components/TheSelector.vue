f63b5c1f07612aafef3060a0b6b07e8e13f805a3
Lucas <lucas.ribeiro711@gmail.com>
2022-10-27 13:30:52 -0300

    fix: change the selector to array

--- src/components/TheSelector.vue
+++ src/components/TheSelector.vue
@@ -2,15 +2,8 @@
   <section class="hidden md:flex flex-col gap-4 flex-1 p-2 mx-auto">
     <img draggable="false" :src="heroImg.src" width="620" class="rounded" />
     <div class="flex justify-around items-center gap-2">
-      <img
-        v-for="image of images"
-        :key="image.src"
-        draggable="false"
-        :src="image.src"
-        :alt="image.alt"
-        class="rounded flex-1 w-36 cursor-pointer"
-        @click="changeImage(image)"
-      />
+      <img v-for="image of images" :key="image.src" draggable="false" :src="image.src" :alt="image.alt"
+        class="rounded flex-1 w-36 cursor-pointer" @click="changeImage(image)" />
     </div>
   </section>
 </template>
@@ -21,11 +14,13 @@ import { defineComponent, ref, reactive } from "vue";
 export default defineComponent({
   setup() {
     let heroImg = reactive({ src: "/images/pvai-app-portfolio.png" });
-    const images = ref({
-      imgOne: { src: "/images/pvai-app-portfolio.png", alt: "Paranavai.app" },
-      imgTwo: { src: "/images/osmarjr-portfolio.png", alt: "Osmar Junior LP" },
-      imgThree: { src: "/images/ieasy-portfolio.png", alt: "Ieasy LP" },
-    });
+
+    const images = ref([
+      { src: "/images/pvai-app-portfolio.png", alt: "Paranavai.app" },
+      { src: "/images/osmarjr-portfolio.png", alt: "Osmar Junior LP" },
+      { src: "/images/ieasy-portfolio.png", alt: "Ieasy LP" },
+    ]);
+
 
     const changeImage = (image: any) => {
       return (heroImg.src = image.src);
