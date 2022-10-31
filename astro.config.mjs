15263f453fa022b72bae88ce368938cdc99b1f98
André Vandal <1340508+andrevandal@users.noreply.github.com>
2022-10-31 16:41:53 -0300

    feat: add robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lenyeg.com.br',
   // ...
-  integrations: [tailwind(), sitemap(), vue()],
+  integrations: [tailwind(), sitemap(), vue(), robotsTxt()],
   vite: {
     ssr: {
       external: ["svgo"],
